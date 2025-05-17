36663e69aec874d45752401dc40c22a41f2adcfb
sdziuba <sdziuba@outlook.com>
2025-05-17 08:48:50 +0300

    origin changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Finance API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["https://finefoliobe.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
