45bb850c1f284f3b5ddf53d9362023e7bd420a58
sdziuba <sdziuba@outlook.com>
2025-05-29 10:03:06 +0200

    origin changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Finance API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://finefolio.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
