c49c7a2ae0b201cc84fce6f5810c9177cbeae589
sdziuba <sdziuba@outlook.com>
2026-01-17 14:09:34 +0100

    bug fix for moex not displaying name

--- services/valuation_service.py
+++ services/valuation_service.py
@@ -158,7 +158,7 @@ def get_moex_stock_data(ticker:str) -> tuple:
        'free_cash_flow_fy', 'debt_to_equity', 'sector_ru', 'industry_ru'])
     data = data[data["Ticker"]==ticker] 
     name = data.iloc[0]['Name']
-    description = ''
+    description = data.iloc[0]['Name']
     exchange = 'MOEX'
     close = get_price_from_moex(ticker=ticker)
     country = 'Russia'
